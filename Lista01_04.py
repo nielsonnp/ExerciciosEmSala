0256a6ad1f7b525fa6d5fd5e559e7b921f51acde
Ranyelson Dantas <nielsonnp@gmail.com>
2018-08-30 09:45:59 -0300

    Lista01_Exercicio04

---
+++ Lista01_04.py
@@ -0,0 +1,6 @@
+nota1 = float(input('Digite Nota1: '))
+nota2 = float(input('Digite Nota2: '))
+nota3 = float(input('Digite Nota3: '))
+nota4 = float(input('Digite Nota4: '))
+
+print('Média =', ((nota1 + nota2 + nota3 + nota4) / 4))
