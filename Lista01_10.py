4e6b6ada8f219be6c4ac718976cd9e5d1c183d09
Ranyelson Dantas <nielsonnp@gmail.com>
2018-08-30 12:47:57 -0300

    Lista01_Exercicio10

---
+++ Lista01_10.py
@@ -0,0 +1,7 @@
+'''aça  um  Programa  que  peça  a  temperatura  em  graus  Farenheit,
+transforme  e  mostre  a  temperatura  em  graus  Celsius.
+C  =  (5  *  (F-32)  /  9)'''
+
+m1 = float(input('Digite a temperatura  em  graus  Farenheit: '))
+
+print('Temperatura = %5.2f' %(5*(m1-32)/9),'°c')
