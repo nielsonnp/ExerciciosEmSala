58cc11bc2cfc3b640bdbe5eb9576ab6969098ac2
Ranyelson Dantas <nielsonnp@gmail.com>
2019-03-11 21:21:49 -0300

    Delete Lista01_10.py

--- Lista01_10.py
+++
@@ -1,7 +0,0 @@
-'''aça  um  Programa  que  peça  a  temperatura  em  graus  Farenheit,
-transforme  e  mostre  a  temperatura  em  graus  Celsius.
-C  =  (5  *  (F-32)  /  9)'''
-
-grau_f = float(input('Digite a temperatura  em  graus  Farenheit: '))
-
-print('Temperatura = %5.2f' %(5*(grau_f-32)/9),'°c')
